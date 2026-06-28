8e53d2ca96a270460f31344324c5f16518893b79
HeldaMounir <helda.mounir@gmail.com>
2026-06-28 21:26:01 +0300

    feat: add toggle form state and add patient button

--- src/app/features/patients/patients.component.ts
+++ src/app/features/patients/patients.component.ts
@@ -7,22 +7,33 @@ import { ApiService } from '../../core/services/api.service';
 import { Patient } from '../../core/models';
 import { SearchInputComponent } from '../../shared/components/search-input.component';
 import { ButtonComponent } from '../../shared/components/button.component';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-patients',
   standalone: true,
-  imports: [CommonModule, SearchInputComponent, ButtonComponent],
+  imports: [CommonModule, ReactiveFormsModule, SearchInputComponent, ButtonComponent],
   templateUrl: './patients.html',
 })
 export class PatientsComponent implements OnInit {
   patients: Patient[] = [];
   filteredPatients: Patient[] = [];
   isLoading = false;
+  isSubmitting = false;
+  showForm = false;
+  patientForm!: FormGroup;
 
-  constructor(private apiService: ApiService) {}
-
+  constructor(
+    private apiService: ApiService,
+    private fb: FormBuilder,
+  ) {}
   ngOnInit(): void {
     this.loadPatients();
+    this.patientForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: [''],
+    });
   }
 
   loadPatients(): void {
@@ -45,5 +56,17 @@ export class PatientsComponent implements OnInit {
     this.filteredPatients = this.patients.filter(
       (p) => p.name.toLowerCase().includes(lower) || p.phone.toLowerCase().includes(lower),
     );
+  }
+  toggleForm(): void {
+    this.showForm = !this.showForm;
+    if (!this.showForm) {
+      this.patientForm.reset();
+    }
+  }
+  get name() {
+    return this.patientForm.get('name');
+  }
+  get phone() {
+    return this.patientForm.get('phone');
   }
 }
